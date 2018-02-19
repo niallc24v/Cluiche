b2fcdf7cf2592e9e08c4c92556cc7a9acec5f75a
niallc24v <36045542+niallc24v@users.noreply.github.com>
2018-02-19 20:13:29 +0000

    Main Menu for game

---
+++ Assets/Scripts/buttonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
